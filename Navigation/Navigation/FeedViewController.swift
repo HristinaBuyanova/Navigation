05028a1fa66b0fc74f96751251e81f112d62969f
HristinaBuyanova <hristina-a@mail.ru>
2022-11-03 20:15:41 +0300

    Добавила PostVC и кнопку для нее

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -12,19 +12,24 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
+        let uiButton = UIButton()
+        self.view.addSubview(uiButton)
+        
+        uiButton.setTitle("Перейти в ленту", for: .normal)
+        uiButton.translatesAutoresizingMaskIntoConstraints = false
+        uiButton.addAction(UIAction(handler: {_ in
+            let vc = PostViewController()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }), for: .touchUpInside)
+       
+        NSLayoutConstraint.activate([
+            uiButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            uiButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            uiButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            uiButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
