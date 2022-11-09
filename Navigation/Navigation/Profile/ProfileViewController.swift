bca06f3f5de276bfe6966bf9ed81dd74d0aad59d
HristinaBuyanova <hristina-a@mail.ru>
2022-11-09 22:41:09 +0300

    Добавила сабвью

--- Navigation/Navigation/Profile/ProfileViewController.swift
+++ Navigation/Navigation/Profile/ProfileViewController.swift
@@ -14,23 +14,19 @@ class ProfileViewController: UIViewController {
         view.backgroundColor = .lightGray
         title = "Profile"
         
-        let profileHeaderView = ProfileHeaderView()
-        view.addSubview(profileHeaderView)
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview()
         
         
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addSubview () {
+        let profileHeaderView = ProfileHeaderView()
+        view.addSubview(profileHeaderView)
+        profileHeaderView.frame = view.frame
+        
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
     }
-    */
+    
 
 }
