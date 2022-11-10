82a3b3745390040e219870f837b528b8b6658fff
HristinaBuyanova <hristina-a@mail.ru>
2022-11-10 13:46:20 +0300

    Добавила UI-элементы согласно макету

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -9,148 +9,102 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    let backgroundView: UIView = {
-            let view = UIView()
-            view.backgroundColor = .lightGray
-            return view
-        }()
+    let profileHW: UIView = {
+         let view = UIView()
+         view.backgroundColor = .lightGray
+         return view
+     }()
     
-    
-    var avatar: UIImageView = {
-        let imageFrame = CGRect(x: 16, y: 16, width: 100, height: 100)
-        let imageView = UIImageView(image: UIImage(named: "avatar.jpg"))
-        imageView.frame = imageFrame
-        imageView.layer.borderWidth = 3.0
+    let avatar: UIImageView = {
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 16, y: 16, width: 100, height: 100)
+        imageView.image = UIImage(named: "avatar")
+        imageView.layer.borderWidth = 3
+        imageView.layer.cornerRadius = imageView.frame.width / 2
         imageView.layer.borderColor = UIColor.white.cgColor
-        imageView.layer.cornerRadius = imageView.frame.width/2
         imageView.clipsToBounds = true
         return imageView
     }()
+        
+    let labelName: UILabel = {
+        let name = UILabel()
+        name.text = "Hipster cat"
+        name.textColor = .black
+        name.font = .systemFont(ofSize: 18, weight: .bold)
+        return name
+    }()
     
-    let name: UILabel = {
-        let label = UILabel()
-        return label
-    } ()
-   
+    let labelStatus: UILabel = {
+        let status = UILabel()
+        status.text = "Waiting for something..."
+        status.textColor = .gray
+        status.font = .systemFont(ofSize: 14, weight: .regular)
+        return status
+    }()
+        
+    let buttonShowStatus: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.setTitle("Show status", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.layer.cornerRadius = 4
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(Any?.self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
+        
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(profileHW)
+        profileHW.addSubview(avatar)
+        profileHW.addSubview(labelName)
+        profileHW.addSubview(labelStatus)
+        profileHW.addSubview(buttonShowStatus)
+        setupView()
+        }
     
-    
-//let avatarFrame = CGRect(x: 16, y: 16, width: 100, height: 100)
-//
-//    var avatar = UIImageView(image: UIImage(named: "avatar.jpg"))
-//
-    
-    
-//    avatar.layer.cornerRadius = 0.5
-//    avatar.layer.borderWidth = 3
-//    avatar.layer.borderColor =
-//
-    
-    
-    
-    
-//    avatar.hitTest(<#T##CGPoint#>, with: <#T##UIEvent?#>)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    func setupView() {
+        
+        profileHW.translatesAutoresizingMaskIntoConstraints = false
+        labelName.translatesAutoresizingMaskIntoConstraints = false
+        buttonShowStatus.translatesAutoresizingMaskIntoConstraints = false
+        labelStatus.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            profileHW.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            profileHW.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            profileHW.widthAnchor.constraint(equalTo: self.widthAnchor),
+            profileHW.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            
+            labelName.leadingAnchor.constraint(equalTo: profileHW.leadingAnchor, constant: 160),
+            labelName.topAnchor.constraint(equalTo: profileHW.topAnchor, constant: 27),
+            
+            labelStatus.leadingAnchor.constraint(equalTo: profileHW.leadingAnchor, constant: 160),
+            labelStatus.bottomAnchor.constraint(equalTo: buttonShowStatus.topAnchor, constant: -34),
+            
+            buttonShowStatus.centerXAnchor.constraint(equalTo: profileHW.centerXAnchor),
+            buttonShowStatus.widthAnchor.constraint(equalTo: profileHW.widthAnchor, constant: -32),
+            buttonShowStatus.heightAnchor.constraint(equalToConstant: 50),
+            buttonShowStatus.topAnchor.constraint(equalTo: profileHW.topAnchor, constant: 132)
+        ])
+    }
+    
+    @objc func buttonPressed() {
+        print(labelStatus.text!)
+    }
+    
 }
-
-
-//let avatarView: UIImageView = {
-//        let avatar = UIImageView()
-//        avatar.frame = CGRect(x: 16, y: 16, width: 120, height: 120)
-//        avatar.layer.borderWidth = 3
-//        avatar.layer.borderColor = UIColor.white.cgColor
-//        avatar.layer.cornerRadius = avatar.frame.width / 2
-//        avatar.image = UIImage(named: "img")
-//        avatar.clipsToBounds = true
-//        return avatar
-//    }()
-//
-//    let labelName: UILabel = {
-//        let label = UILabel()
-//        label.text = "Hipster cat"
-//        label.font = .systemFont(ofSize: 18, weight: .bold)
-//        return label
-//    }()
-//
-//    let buttonStatus: UIButton = {
-//        let button = UIButton()
-//        button.backgroundColor = .systemBlue
-//        button.setTitle("Show status", for: .normal)
-//        button.setTitleColor(UIColor.white, for: .normal)
-//        button.layer.cornerRadius = 4
-//        button.layer.shadowOffset = CGSize(width: 4, height: 4)
-//        button.layer.shadowRadius = 4
-//        button.layer.shadowColor = UIColor.black.cgColor
-//        button.layer.shadowOpacity = 0.7
-//        button.addTarget(Any?.self, action: #selector(buttonStatusAction), for: .touchUpInside)
-//        return button
-//    }()
-//
-//    let labelStatus: UILabel = {
-//        let status = UILabel()
-//        status.text = "Waiting for something..."
-//        status.font = .systemFont(ofSize: 14, weight: .regular)
-//        status.textColor = .gray
-//        return status
-//    }()
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        self.addSubview(mainView)
-//        mainView.addSubview(avatarView)
-//        mainView.addSubview(labelName)
-//        mainView.addSubview(buttonStatus)
-//        setupView()
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    func setupView() {
-//        mainView.translatesAutoresizingMaskIntoConstraints = false
-//        labelName.translatesAutoresizingMaskIntoConstraints = false
-//        buttonStatus.translatesAutoresizingMaskIntoConstraints = false
-//        labelStatus.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            mainView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-//            mainView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-//            mainView.widthAnchor.constraint(equalTo: self.widthAnchor),
-//            mainView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
-//
-//            labelName.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: 160),
-//            labelName.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 27),
-//
-//            buttonStatus.centerXAnchor.constraint(equalTo: mainView.centerXAnchor),
-//            buttonStatus.widthAnchor.constraint(equalTo: mainView.widthAnchor, constant: -32),
-//            buttonStatus.heightAnchor.constraint(equalToConstant: 50),
-//            buttonStatus.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 152)
-//        ])
-//    }
-//
-//    @objc func buttonStatusAction() {
-//        mainView.addSubview(labelStatus)
-//        labelStatus.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            labelStatus.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: 160),
-//            labelStatus.bottomAnchor.constraint(equalTo: buttonStatus.topAnchor, constant: -34)
-//        ])
-//        print(labelStatus.text!)
-//    }
-//
-//}
+    
+    
+    
+    
+    
+ 
--- Navigation/Navigation/Profile/ProfileViewController.swift
+++ Navigation/Navigation/Profile/ProfileViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     
     let profileHeaderView = ProfileHeaderView()
     
@@ -16,16 +16,12 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Profile"
-        
         view.addSubview(profileHeaderView)
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        
     }
-    
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         profileHeaderView.frame = view.frame
     }
-
+    
 }
